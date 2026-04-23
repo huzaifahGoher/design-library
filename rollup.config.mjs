e460f169c34385650a7dc29963e3a852b2120f9b
Huzaifah Goher <huzaifah191001@gmail.com>
2026-04-23 20:07:44 +0500

    module changes.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,10 +4,10 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-  input: "src/index.ts",
-  output: [
-    { file: "dist/index.js", format: "cjs", sourcemap: true },
-    { file: "dist/index.esm.js", format: "esm", sourcemap: true },
-  ],
-  plugins: [peerDepsExternal(), resolve(), commonjs(), typescript({ useTsconfigDeclarationDir: true })],
+    input: "src/index.ts",
+    output: [
+        { file: "dist/index.js", format: "cjs", sourcemap: true },
+        { file: "dist/index.esm.mjs", format: "esm", sourcemap: true },
+    ],
+    plugins: [peerDepsExternal(), resolve(), commonjs(), typescript({ useTsconfigDeclarationDir: true })],
 };
