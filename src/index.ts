0c7e3738a1a26711173c3a47fdbbb3ce5cf86c5a
Huzaifah Goher <huzaifah191001@gmail.com>
2026-05-09 18:44:50 +0500

    theme overhaul

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export {Button} from "./components/Button";
 export {ThemeProvider, useTheme } from './theme/ThemeProvider';
-export { defaultTheme, defaultLightTheme, defaultDarkTheme } from "./theme/defaulttheme";
+export { semantic, darkSemantic } from "./theme/defaulttheme";
 export type { ThemeTypes } from './types/theme/themetypes';
