c3bb9210792aa4969bc34b15b4fa651f98bb9e4c
Huzaifah Goher <huzaifah191001@gmail.com>
2026-05-10 17:18:41 +0500

    exported Select from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export {Button} from "./components/Button";
+export {Select} from "./components/Select";
 export {ThemeProvider, useTheme } from './theme/ThemeProvider';
 export { semantic, darkSemantic } from "./theme/defaulttheme";
 export type { ThemeTypes } from './types/theme/themetypes';
